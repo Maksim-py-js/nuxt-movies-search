2a5a82b0d54cfde8dacd431c6e63090bc7ddefac
unknown <you@example.com>
2020-05-05 13:09:07 +0500

    Create project

--- pages/index.vue
+++ pages/index.vue
@@ -1,65 +1,49 @@
 <template>
-  <section class="container">
-    <div>
-      <app-logo/>
-      <h1 class="title">
-        nuxt-movies-search
-      </h1>
-      <h2 class="subtitle">
-        Nuxt.js project
-      </h2>
-      <div class="links">
-        <a
-          href="https://nuxtjs.org/"
-          target="_blank"
-          class="button--green">Documentation</a>
-        <a
-          href="https://github.com/nuxt/nuxt.js"
-          target="_blank"
-          class="button--grey">GitHub</a>
-      </div>
-    </div>
-  </section>
+  <div>
+    <h1 class="title">Search movies</h1>
+    <div class="input"> <input type="text" placeholder="Input name Movie"></div>
+    <div class="btn"><button>Search</button></div>
+
+  </div>
 </template>
 
-<script>
-import AppLogo from '~/components/AppLogo.vue'
-
-export default {
-  components: {
-    AppLogo
+<style scoped>
+  .title {
+    padding-top: 50px;
+    padding-bottom: 30px;
+    text-align: center;
+    text-transform: uppercase;
   }
-}
-</script>
-
-<style>
-.container {
-  min-height: 100vh;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  text-align: center;
-}
-
-.title {
-  font-family: "Quicksand", "Source Sans Pro", -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, "Helvetica Neue", Arial, sans-serif; /* 1 */
-  display: block;
-  font-weight: 300;
-  font-size: 100px;
-  color: #35495e;
-  letter-spacing: 1px;
-}
-
-.subtitle {
-  font-weight: 300;
-  font-size: 42px;
-  color: #526488;
-  word-spacing: 5px;
-  padding-bottom: 15px;
-}
-
-.links {
-  padding-top: 15px;
-}
+  .input {
+    width: 600px;
+    margin: 0 auto;
+  }
+  input {
+    width: 600px;
+    text-align: center;
+    text-transform: uppercase;
+    font-size: 15px;
+    font-family: 'Roboto';
+    font-weight: bold;
+    color: #000;
+    border-radius: 20px;
+    padding: 10px;
+    margin-bottom: 30px;
+  }
+  .btn {
+    width: 150px;
+    margin: 0 auto;
+  }
+  .btn button {
+    padding: 20px;
+    background: skyblue;
+    color: #fff;
+    text-transform: uppercase;
+    border: 1px solid white;
+    border-radius: 20px;
+    width: 150px;
+    outline: none;
+    cursor: pointer;
+  }
 </style>
 
